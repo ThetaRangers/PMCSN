3089b6af26a19f3d24d11267b4af8c40819894ee
Lorenzo Valeriani <lorenzoval98@gmail.com>
2021-08-20 10:13:44 +0200

    Reformat code
    Signed-off-by: Lorenzo Valeriani <lorenzoval98@gmail.com>

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "rngs.h" 
+#include "rngs.h"
 #include "rvgs.h"
 
 int main() {
 	double test;
-	SelectStream(0);                  /* select the default stream */
+	SelectStream(0); /* select the default stream */
 	PutSeed(1);
 	test = Exponential(6);
 	printf("Numero: %lf\n", test);
--- rvgs.c
+++ rvgs.c
@@ -37,118 +37,117 @@
 #include "rngs.h"
 #include "rvgs.h"
 
-
-   long Bernoulli(double p)
+long Bernoulli(double p)
 /* ========================================================
  * Returns 1 with probability p or 0 with probability 1 - p. 
  * NOTE: use 0.0 < p < 1.0                                   
  * ========================================================
- */ 
+ */
 {
-  return ((Random() < (1.0 - p)) ? 0 : 1);
+	return ((Random() < (1.0 - p)) ? 0 : 1);
 }
 
-   long Binomial(long n, double p)
+long Binomial(long n, double p)
 /* ================================================================ 
  * Returns a binomial distributed integer between 0 and n inclusive. 
  * NOTE: use n > 0 and 0.0 < p < 1.0
  * ================================================================
  */
-{ 
-  long i, x = 0;
+{
+	long i, x = 0;
 
-  for (i = 0; i < n; i++)
-    x += Bernoulli(p);
-  return (x);
+	for (i = 0; i < n; i++)
+		x += Bernoulli(p);
+	return (x);
 }
 
-   long Equilikely(long a, long b)
+long Equilikely(long a, long b)
 /* ===================================================================
  * Returns an equilikely distributed integer between a and b inclusive. 
  * NOTE: use a < b
  * ===================================================================
  */
 {
-  return (a + (long) ((b - a + 1) * Random()));
+	return (a + (long)((b - a + 1) * Random()));
 }
 
-   long Geometric(double p)
+long Geometric(double p)
 /* ====================================================
  * Returns a geometric distributed non-negative integer.
  * NOTE: use 0.0 < p < 1.0
  * ====================================================
  */
 {
-  return ((long) (log(1.0 - Random()) / log(p)));
+	return ((long)(log(1.0 - Random()) / log(p)));
 }
 
-   long Pascal(long n, double p)
+long Pascal(long n, double p)
 /* ================================================= 
  * Returns a Pascal distributed non-negative integer. 
  * NOTE: use n > 0 and 0.0 < p < 1.0
  * =================================================
  */
-{ 
-  long i, x = 0;
+{
+	long i, x = 0;
 
-  for (i = 0; i < n; i++)
-    x += Geometric(p);
-  return (x);
+	for (i = 0; i < n; i++)
+		x += Geometric(p);
+	return (x);
 }
 
-   long Poisson(double m)
+long Poisson(double m)
 /* ================================================== 
  * Returns a Poisson distributed non-negative integer. 
  * NOTE: use m > 0
  * ==================================================
  */
-{ 
-  double t = 0.0;
-  long   x = 0;
+{
+	double t = 0.0;
+	long x = 0;
 
-  while (t < m) {
-    t += Exponential(1.0);
-    x++;
-  }
-  return (x - 1);
+	while (t < m) {
+		t += Exponential(1.0);
+		x++;
+	}
+	return (x - 1);
 }
 
-   double Uniform(double a, double b)
+double Uniform(double a, double b)
 /* =========================================================== 
  * Returns a uniformly distributed real number between a and b. 
  * NOTE: use a < b
  * ===========================================================
  */
-{ 
-  return (a + (b - a) * Random());
+{
+	return (a + (b - a) * Random());
 }
 
-   double Exponential(double m)
+double Exponential(double m)
 /* =========================================================
  * Returns an exponentially distributed positive real number. 
  * NOTE: use m > 0.0
  * =========================================================
  */
 {
-  return (-m * log(1.0 - Random()));
+	return (-m * log(1.0 - Random()));
 }
 
-   double Erlang(long n, double b)
+double Erlang(long n, double b)
 /* ================================================== 
  * Returns an Erlang distributed positive real number.
  * NOTE: use n > 0 and b > 0.0
  * ==================================================
  */
-{ 
-  long   i;
-  double x = 0.0;
+{
+	long i;
+	double x = 0.0;
 
-  for (i = 0; i < n; i++) 
-    x += Exponential(b);
-  return (x);
+	for (i = 0; i < n; i++)
+		x += Exponential(b);
+	return (x);
 }
 
-   double Normal(double m, double s)
+double Normal(double m, double s)
 /* ========================================================================
  * Returns a normal (Gaussian) distributed real number.
  * NOTE: use s > 0.0
@@ -157,62 +156,66 @@
  * J. Applied Statistics, 1974, vol 23, pp 96-97.
  * ========================================================================
  */
-{ 
-  const double p0 = 0.322232431088;     const double q0 = 0.099348462606;
-  const double p1 = 1.0;                const double q1 = 0.588581570495;
-  const double p2 = 0.342242088547;     const double q2 = 0.531103462366;
-  const double p3 = 0.204231210245e-1;  const double q3 = 0.103537752850;
-  const double p4 = 0.453642210148e-4;  const double q4 = 0.385607006340e-2;
-  double u, t, p, q, z;
+{
+	const double p0 = 0.322232431088;
+	const double q0 = 0.099348462606;
+	const double p1 = 1.0;
+	const double q1 = 0.588581570495;
+	const double p2 = 0.342242088547;
+	const double q2 = 0.531103462366;
+	const double p3 = 0.204231210245e-1;
+	const double q3 = 0.103537752850;
+	const double p4 = 0.453642210148e-4;
+	const double q4 = 0.385607006340e-2;
+	double u, t, p, q, z;
 
-  u   = Random();
-  if (u < 0.5)
-    t = sqrt(-2.0 * log(u));
-  else
-    t = sqrt(-2.0 * log(1.0 - u));
-  p   = p0 + t * (p1 + t * (p2 + t * (p3 + t * p4)));
-  q   = q0 + t * (q1 + t * (q2 + t * (q3 + t * q4)));
-  if (u < 0.5)
-    z = (p / q) - t;
-  else
-    z = t - (p / q);
-  return (m + s * z);
+	u = Random();
+	if (u < 0.5)
+		t = sqrt(-2.0 * log(u));
+	else
+		t = sqrt(-2.0 * log(1.0 - u));
+	p = p0 + t * (p1 + t * (p2 + t * (p3 + t * p4)));
+	q = q0 + t * (q1 + t * (q2 + t * (q3 + t * q4)));
+	if (u < 0.5)
+		z = (p / q) - t;
+	else
+		z = t - (p / q);
+	return (m + s * z);
 }
 
-   double Lognormal(double a, double b)
+double Lognormal(double a, double b)
 /* ==================================================== 
  * Returns a lognormal distributed positive real number. 
  * NOTE: use b > 0.0
  * ====================================================
  */
 {
-  return (exp(a + b * Normal(0.0, 1.0)));
+	return (exp(a + b * Normal(0.0, 1.0)));
 }
 
-   double Chisquare(long n)
+double Chisquare(long n)
 /* =====================================================
  * Returns a chi-square distributed positive real number. 
  * NOTE: use n > 0
  * =====================================================
  */
-{ 
-  long   i;
-  double z, x = 0.0;
+{
+	long i;
+	double z, x = 0.0;
 
-  for (i = 0; i < n; i++) {
-    z  = Normal(0.0, 1.0);
-    x += z * z;
-  }
-  return (x);
+	for (i = 0; i < n; i++) {
+		z = Normal(0.0, 1.0);
+		x += z * z;
+	}
+	return (x);
 }
 
-   double Student(long n)
+double Student(long n)
 /* =========================================== 
  * Returns a student-t distributed real number.
  * NOTE: use n > 0
  * ===========================================
  */
 {
-  return (Normal(0.0, 1.0) / sqrt(Chisquare(n) / n));
+	return (Normal(0.0, 1.0) / sqrt(Chisquare(n) / n));
 }
-
