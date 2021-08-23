7c30ee6f0de7739873a620c1c14e4c644431fc39
ferra-rally <danieleferrarelli@gmail.com>
2021-08-23 11:38:10 +0200

    Added income

--- main.c
+++ main.c
@@ -19,8 +19,12 @@
 #define ONLINE_PERC 0.6
 #define CHECK_PERC 0.4
 
+#define FIRST_CLASS_SPENDING 20
+#define SECOND_CLASS_SPENDING 5
+
 enum stream { ARRIVAL, SERVICE, ROUTING };
 
+int income = 0;
 int febbra = 0;
 int online = 0;
 int dropoff = 0;
@@ -28,17 +32,15 @@ int normal = 0;
 
 int nodesNumber = TEMP_NODE + CHECK_NODE + SECURITY_NODE + DROPOFF_ONLINE;
 
-void findEvent()
-{
-}
-
 int getDestination(enum node_type type)
 {
 	double rand;
 	switch (type) {
 	case TEMP:
+		SelectStream(253);
 		return Equilikely(0, TEMP_NODE - 1);
 	case CHECK:
+		SelectStream(252);
 		rand = Random();
 		if (rand < CHECK_PERC * (1 - FEVER_PERC)) {
 			rand = rand/CHECK_PERC;
@@ -60,6 +62,7 @@ int getDestination(enum node_type type)
 
 		return destination;
 	case SECURITY:
+		SelectStream(251);
 		return Equilikely(TEMP_NODE + CHECK_NODE, TEMP_NODE + CHECK_NODE + SECURITY_NODE - 1);
 	default:
 		return 0;
@@ -70,26 +73,23 @@ double getArrival()
 {
 	static double arrival = START;
 
-	SelectStream(0);
-	arrival += Exponential(2.0); //Change this
+	SelectStream(254);
+	arrival += Exponential(0.02);
 	return (arrival);
 }
 
-double getService(enum node_type type)
+double getService(enum node_type type, int id)
 {
+	SelectStream(id);
 	switch (type) {
 	case TEMP:
-		SelectStream(1);
-		return (Erlang(5, 0.3)); //Change
+		return Exponential(0.5);
 	case CHECK:
-		SelectStream(1);
-		return (Erlang(5, 0.3)); //Change
+		return Normal(0.2, 0.1);
 	case SECURITY:
-		SelectStream(1);
-		return (Erlang(5, 0.3)); //Change
+		return (Erlang(5, 0.3));
 	case DROP_OFF:
-		SelectStream(1);
-		return (Erlang(5, 0.3)); //Change
+		return Normal(1, 0.1);
 	default:
 		return 0;
 	}
@@ -136,8 +136,6 @@ int main()
 	int povery = 0;
 	int tot_arrivals = 0;
 
-	//SINGOLA TEMPURADURA
-	// || (number > 0)
 	int id;
 	while ((t.arrival < STOP) || number > 0) {
 		double minCompletion = minNode(nodes, nodesNumber, &id);
@@ -164,7 +162,7 @@ int main()
 			if (nodes[destination].number == 1)
 				nodes[destination].completion =
 					t.current +
-					getService(nodes[destination].type);
+					getService(nodes[destination].type, destination);
 
 		} else {
 			//Servizietto
@@ -179,7 +177,7 @@ int main()
 
 				if (nodes[id].number > 0)
 					nodes[id].completion =
-						t.current + getService(nodes[id].type);
+						t.current + getService(nodes[id].type, id);
 				else
 					nodes[id].completion = INFINITY;
 
@@ -189,7 +187,7 @@ int main()
 					nodes[destination].number++;
 					enqueue(&nodes[destination].head,&nodes[destination].tail, pass_type);
 					if (nodes[destination].number == 1)
-						nodes[destination].completion = t.current + getService(nodes[destination].type);
+						nodes[destination].completion = t.current + getService(nodes[destination].type, destination);
 				} else {
 					number--;
 				}
@@ -201,7 +199,7 @@ int main()
 				
 				if (nodes[id].number > 0)
 					nodes[id].completion =
-						t.current + getService(nodes[id].type);
+						t.current + getService(nodes[id].type, id);
 				else
 					nodes[id].completion = INFINITY;
 				
@@ -210,21 +208,23 @@ int main()
 				nodes[destination].number++;
 				enqueue(&nodes[destination].head,&nodes[destination].tail, pass_type);
 				if (nodes[destination].number == 1)
-					nodes[destination].completion = t.current + getService(nodes[destination].type);
+					nodes[destination].completion = t.current + getService(nodes[destination].type, destination);
 
 				break;
 			case SECURITY:
 				number--;
 				pass_type = dequeue(&nodes[id].head, &nodes[id].tail);
 				if (pass_type == FIRST_CLASS) {
+					income += FIRST_CLASS_SPENDING;
 					rikky++;
 				} else {
+					income += SECOND_CLASS_SPENDING;
 					povery++;
 				}
 				
 				if (nodes[id].number > 0)
 					nodes[id].completion =
-						t.current + getService(nodes[id].type);
+						t.current + getService(nodes[id].type, id);
 				else
 					nodes[id].completion = INFINITY;
 
@@ -235,5 +235,7 @@ int main()
 
 	printf("Ricchi: %d , Poveri: %d, Tot Arrivi: %d, Sum completion: %d\n", rikky, povery, tot_arrivals, rikky + povery);
 	printf("Febbra: %d, Online: %d, Normale: %d, Dropoff: %d\n", febbra, online, normal, dropoff);
+	printf("Income: %d\n", income);
+
 	return 0;
 }
